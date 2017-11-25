c1d8c1e66497ee8cf9b55ea6f65a2aeb4f4cf377
Catalin Hutanu <catalin@hutanu.ro>
2017-11-26 00:26:47 +0200

    - index and search actions

--- src/Framework/Controller/AppController.php
+++ src/Framework/Controller/AppController.php
@@ -4,14 +4,27 @@ namespace App\Framework\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AppController extends Controller
 {
     /**
+     * @Route("/")
+     *
      * @return array
      */
     public function indexAction()
     {
         return [];
+    }
+
+    /**
+     * @Route("/search")
+     *
+     * @return JsonResponse
+     */
+    public function searchAction()
+    {
+        return new JsonResponse([]);
     }
 }
--- tests/Framework/Controller/AppControllerTest.php
+++ tests/Framework/Controller/AppControllerTest.php
@@ -4,21 +4,37 @@ namespace App\Tests\Framework\Controller;
 
 use App\Framework\Controller\AppController;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AppControllerTest extends TestCase
 {
     /** @var AppController */
     private $controller;
 
+    /**
+     * Setup tests
+     */
     public function setUp()
     {
         $this->controller = new AppController();
-
     }
 
+    /**
+     * Test that index returns empty
+     */
     public function testEmptyIndexAction()
     {
-        $indexResponse = $this->controller->indexAction();
-        $this->assertEmpty($indexResponse);
+        $response = $this->controller->indexAction();
+        $this->assertEmpty($response);
+    }
+
+    /**
+     * Test that search returns JsonResponse
+     */
+    public function testSearchAction()
+    {
+        $response = $this->controller->searchAction();
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
     }
 }
